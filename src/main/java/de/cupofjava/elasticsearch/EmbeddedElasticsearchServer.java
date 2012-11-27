ce1a8b406d3359f8c65453579c48afc9e53d2e8f
Felix Müller <felix.mueller.berlin@googlemail.com>
2012-11-27 12:20:17 +0100

    added example for integration tests with an embedded elasticsearch server

---
+++ src/main/java/de/cupofjava/elasticsearch/EmbeddedElasticsearchServer.java
@@ -0,0 +1,56 @@
+package de.cupofjava.elasticsearch;
+
+import org.apache.commons.io.FileUtils;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.node.Node;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+/**
+ * Example implementation of an embedded elasticsearch server.
+ *
+ * @author Felix Müller
+ */
+public class EmbeddedElasticsearchServer {
+
+    private static final String DEFAULT_DATA_DIRECTORY = "target/elasticsearch-data";
+
+    private final Node node;
+
+    public EmbeddedElasticsearchServer() {
+        this(DEFAULT_DATA_DIRECTORY);
+    }
+
+    public EmbeddedElasticsearchServer(String dataDirectory) {
+        ImmutableSettings.Builder elasticsearchSettings = ImmutableSettings.settingsBuilder()
+                .put("http.enabled", "false")
+                .put("path.data", dataDirectory);
+        node = nodeBuilder()
+                .local(true)
+                .settings(elasticsearchSettings.build())
+                .node();
+
+    }
+
+    public Client getClient() {
+        return node.client();
+    }
+
+    public void shutdown() {
+        node.close();
+        deleteDataDirectory();
+    }
+
+    private void deleteDataDirectory() {
+        File dataDirectory = new File(DEFAULT_DATA_DIRECTORY);
+        try {
+            FileUtils.deleteDirectory(dataDirectory);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not delete data directory of embedded elasticsearch server", e);
+        }
+    }
+}
