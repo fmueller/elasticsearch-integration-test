7e00c1fc09251e4535fa2cbc50e51abb11e845b2
Felix Müller <felix.mueller.berlin@googlemail.com>
2012-11-27 12:56:53 +0100

    fixed cleanup of data directory

--- src/main/java/de/cupofjava/elasticsearch/EmbeddedElasticsearchServer.java
+++ src/main/java/de/cupofjava/elasticsearch/EmbeddedElasticsearchServer.java
@@ -20,15 +20,19 @@ public class EmbeddedElasticsearchServer {
     private static final String DEFAULT_DATA_DIRECTORY = "target/elasticsearch-data";
 
     private final Node node;
+    private final String dataDirectory;
 
     public EmbeddedElasticsearchServer() {
         this(DEFAULT_DATA_DIRECTORY);
     }
 
     public EmbeddedElasticsearchServer(String dataDirectory) {
+        this.dataDirectory = dataDirectory;
+
         ImmutableSettings.Builder elasticsearchSettings = ImmutableSettings.settingsBuilder()
                 .put("http.enabled", "false")
                 .put("path.data", dataDirectory);
+
         node = nodeBuilder()
                 .local(true)
                 .settings(elasticsearchSettings.build())
@@ -45,9 +49,8 @@ public class EmbeddedElasticsearchServer {
     }
 
     private void deleteDataDirectory() {
-        File dataDirectory = new File(DEFAULT_DATA_DIRECTORY);
         try {
-            FileUtils.deleteDirectory(dataDirectory);
+            FileUtils.deleteDirectory(new File(dataDirectory));
         } catch (IOException e) {
             throw new RuntimeException("Could not delete data directory of embedded elasticsearch server", e);
         }
